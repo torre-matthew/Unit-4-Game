ac3978226784176d28a155d6db8283ec100d8d2a
Torre Matthew <torre.matthew@gmail.com>
2018-08-14 22:21:28 -0400

    crystals now have a value that is randomized, clicking crystals update scores, a bit more styling

--- assets/javascript/crystal.js
+++ assets/javascript/crystal.js
@@ -1,5 +1,4 @@
-
-
+$("document").ready(function (){
 // give each crystal a randomly generated number between 1-6
 let blueVal = Math.floor((Math.random()*6) + 1);
 let greenVal = Math.floor((Math.random()*6) + 1);
@@ -14,11 +13,70 @@ let clicks4 = Math.floor((Math.random()*3) + 1);
 // Have the randum number that user is trying to guess display as the sum of the equation above
 let targetNum = blueVal*clicks1 + greenVal*clicks2 + purpleVal*clicks3 + redVal*clicks4;
 
-console.log(targetNum);
+let userTotal = 0;
+let wins = 0;
+let losses = 0;
+console.log("blue " + blueVal);
+console.log("green " + greenVal);
+console.log("purple " + purpleVal);
+console.log("red " + redVal);
 
 $("#randnum").text(targetNum);
+$("#user-total").text(userTotal);
+$(".wins").text(wins);
+$(".losses").text(losses);
 
-// Assign values to the crystals
+//These four click handlers assign values to the crystals and have them change the total score when clicked.
+$(".crystal-blue").on("click", function () {
+userTotal += blueVal;
+$("#user-total").text(userTotal);
+if (userTotal === targetNum) {
+    $("#user-total").text(userTotal);
+    alert("yous a bad mothafucker");
+}else if (userTotal > targetNum) {
+    alert("LOSER!!!!!");
+}
+});
+
+$(".crystal-green").on("click", function () {
+userTotal += greenVal;
+$("#user-total").text(userTotal);
+if (userTotal === targetNum) {
+    $("#user-total").text(userTotal);
+    alert("yous a bad mothafucker");
+}else if (userTotal > targetNum) {
+    alert("LOSER!!!!!");
+}
+});
+
+$(".crystal-purple").on("click", function () {
+userTotal += purpleVal;
+$("#user-total").text(userTotal);
+if (userTotal === targetNum) {
+    $("#user-total").text(userTotal);
+    alert("yous a bad mothafucker");
+}else if (userTotal > targetNum) {
+    alert("LOSER!!!!!");
+}
+});
+
+$(".crystal-red").on("click", function () {
+userTotal += redVal;
+$("#user-total").text(userTotal);
+if (userTotal === targetNum) {
+    $("#user-total").text(userTotal);
+    alert("yous a bad mothafucker");
+}else if (userTotal > targetNum) {
+    alert("LOSER!!!!!");
+}
+});
+ 
+
+
+
+
+
+
 // clicking a crystal displays the value of that crystal on the page
 // value of the crystals are added together and displayed on the page
     //this will need to show the number incrementing as crystals are clicked
@@ -27,3 +85,5 @@ $("#randnum").text(targetNum);
     // reset everything and ++ the win in this scenario
 // if the sum this calculated as the user clicks a crystal is greater than randum number they lose
     // reset everthing and ++ loses in this scenario.
+
+});
